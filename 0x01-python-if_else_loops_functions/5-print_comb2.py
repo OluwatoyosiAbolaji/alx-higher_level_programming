b637a231d6478e68c9bbdd20a1742849a6a82a4a
OluwatoyosiAbolaji <oluwatoyosi.abolaji25@gmail.com>
2023-08-08 22:40:10 +0100

    update

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 number = 0
 for number in range(100):
     if number == 99:
-        print(f'{number:02d}')
+        print('{:02d}'.format(number))
     else:
-        print(f'{number:02d},', end=' ')
+        print('{:02d},'.format(number), end=' ')
