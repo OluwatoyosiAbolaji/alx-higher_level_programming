697d03df33f92323caef529d70906493eac30361
OluwatoyosiAbolaji <oluwatoyosi.abolaji25@gmail.com>
2023-08-08 22:57:38 +0100

    update

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-number = 0
-for number in range(100):
-    if number == 99:
-        print('{:02d}'.format(number))
+for i in range(0, 100):
+    if i != 99:
+        print("{:02d}, ".format(i), end="")
     else:
-        print('{:02d},'.format(number), end=' ')
+        print("{:02d}".format(i))
