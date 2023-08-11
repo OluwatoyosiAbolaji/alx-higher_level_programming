dba55f8a706282c5489922166e9cd72509f3dadb
OluwatoyosiAbolaji <oluwatoyosi.abolaji25@gmail.com>
2023-08-11 22:21:56 +0100

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,11 @@ if (__name__ == "__main__"):
     argv = sys.argv
     arg_length = len(argv) - 1
     if (arg_length > 1):
-	    print("{:d} arguments:".format(arg_length))
+        print("{:d} arguments:".format(arg_length))
     elif (arg_length == 1):
-            print("{:d} argument:".format(arg_length))
+        print("{:d} argument:".format(arg_length))
     else:
-            print("{:d} argument.".format(arg_length))
+        print("{:d} argument.".format(arg_length))
     if (arg_length >= 1):
         for arg in range(1, (arg_length + 1)):
             print("{:d}: {}".format(arg, argv[arg]))
