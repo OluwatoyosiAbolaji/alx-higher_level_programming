d2eb45a9210662c04c93112fc0e365cac5bd7de9
OluwatoyosiAbolaji <oluwatoyosi.abolaji25@gmail.com>
2023-09-02 20:53:17 +0100

    update

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a_length = len(tuple_a)
-    tuple_b_length = len(tuple_b)
-    tuple_a = list(tuple_a)
-    tuple_b = list(tuple_b)
-    if tuple_a_length == 0:
-        tuple_a = [0, 0]
-    elif tuple_b_length == 0:
-        tuple_b = [0, 0]
-    elif tuple_a_length == 1:
-        tuple_a = [tuple_a[0], 0]
-    elif tuple_b_length == 1:
-        tuple_b = [tuple_b[0], 0]
-    new_tuple = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+    list_a = list(tuple_a)
+    list_b = list(tuple_b)
+    new_list = []
+    if len(tuple_a) == 0:
+        list_a.append(0)
+        list_a.append(0)
+    elif len(tuple_a) == 1:
+        list_a.append(0)
+    if len(tuple_b) == 0:
+        list_b.append(0)
+        list_b.append(0)
+    elif len(tuple_b) == 1:
+        list_b.append(0)
+    new_list.append(list_a[0] + list_b[0])
+    new_list.append(list_a[1] + list_b[1])
+    new_tuple = tuple(new_list)
     return new_tuple
