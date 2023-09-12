ed0ce314d504e2176c1a574f02e67b90f31285f3
OluwatoyosiAbolaji <oluwatoyosi.abolaji25@gmail.com>
2023-09-12 01:33:58 +0100

    update

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -18,7 +18,7 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if type(self.__size) != int:
+        if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must >= 0")
