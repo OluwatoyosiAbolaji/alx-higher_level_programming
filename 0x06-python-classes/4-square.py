de5bc9d96ee18b5de868e5e815da9da7b38e4fcd
OluwatoyosiAbolaji <oluwatoyosi.abolaji25@gmail.com>
2023-09-12 01:28:58 +0100

    update

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" This method contains a Square class """
+
+
+class Square:
+    """ This class contains methods and fields for Square
+
+    Args:
+        size (int): size of the Square
+        value (int): new size of the Square
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
