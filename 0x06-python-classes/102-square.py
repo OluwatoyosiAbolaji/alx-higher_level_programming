5b317f32c0069e6c42e22db470a78db5c452d0aa
OluwatoyosiAbolaji <oluwatoyosi.abolaji25@gmail.com>
2023-09-13 16:30:48 +0100

    uppdate

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" This method contains the square class """
+
+
+class Square:
+    """ This contains fields and exceptions for a Square class
+
+    Args:
+        size (int): size/length of a square
+        value (int): new value of size
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.size ** 2
+
+    def __eq__(self, other):
+        return self.size == other.size
+
+    def __ne__(self, other):
+        return self.size != other.size
+
+    def __gt__(self, other):
+        return self.size > other.size
+
+    def __ge__(self, other):
+        return self.size >= other.size
+
+    def __lt__(self, other):
+        return self.size < other.size
+
+    def __le__(self, other):
+        return self.size <= other.size
